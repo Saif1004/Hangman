5af53193d75be7ad808927d7a08f30f16c6b07d7
Saif1004 <s.shah7944@student.leedsbeckett.ac.uk>
2023-11-01 02:06:52 +0000

    Save 2

--- CALC.py
+++ CALC.py
@@ -2,28 +2,46 @@ import random
 from words import words
 import string
 
-def get_valid_letter(words):
+def get_valid_word(words):
     word = random.choice(words)
-    while "-" in word or " ":
+    while '-' in word or ' ' in word:
         word = random.choice(words)
 
-    return word.upper()
+    return word
 
 def hangman():
-    alphabet = set(string.ascii_uppercase)
-    word = get_valid_letter(words)
+    word = get_valid_word(words)
+    alphabet = set(string.ascii_lowercase)
     word_letters = set(word)
     used_letters = set()
 
-    user_letter = ("Guess a letter").upper()
-    if user_letter in alphabet - used_letters:
-        print("Correct")
-        used_letters.add(user_letter)
-    if user_letter in word_letters:
-        word_letters.remove(user_letter)
-    elif user_letter in used_letters:
-        print("You have already used this letter")
+    while len(word_letters) > 0:
+        print('You have used these letters:' , '' .join(used_letters).upper())
 
+
+
+
+        word_list = [letter if letter in used_letters else '-' for letter in word]
+        print('Current word: ', ' '.join(word_list).upper())
+
+        user_letter = input('Guess a letter: ').lower()
+
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)
+        if user_letter in word_letters:
+            word_letters.remove(user_letter)
+        elif user_letter in used_letters:
+            print("You have already used this letter")
+        else:
+            print("Invalid")
+
+
+
+
+
+
+
+hangman()
 
 
 
