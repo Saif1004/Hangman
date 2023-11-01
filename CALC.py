05cf4f32ca6a8eb8f205d15349ed11571d736794
Saif1004 <s.shah7944@student.leedsbeckett.ac.uk>
2023-11-01 01:15:00 +0000

    Save 2

--- CALC.py
+++ CALC.py
@@ -1,5 +1,6 @@
 import random
 from words import words
+import string
 
 def get_valid_letter(words):
     word = random.choice(words)
@@ -8,6 +9,29 @@ def get_valid_letter(words):
 
     return word.upper()
 
-def hangman()
+def hangman():
+    alphabet = set(string.ascii_uppercase)
+    word = get_valid_letter(words)
+    word_letters = set(word)
+    used_letters = set()
+
+    user_letter = ("Guess a letter").upper()
+    if user_letter in alphabet - used_letters:
+        print("Correct")
+        used_letters.add(user_letter)
+    if user_letter in word_letters:
+        word_letters.remove(user_letter)
+    elif user_letter in used_letters:
+        print("You have already used this letter")
+
+
+
+
+
+
+
+
+
+
 
 
