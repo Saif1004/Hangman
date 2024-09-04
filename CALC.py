1ba1073979bda4d9a5611072d88d09777dc913f7
Saif1004 <s.shah7944@student.leedsbeckett.ac.uk>
2024-09-04 21:38:13 +0100

    Updated

--- CALC.py
+++ CALC.py
@@ -25,15 +25,16 @@ def hangman():
         print('Current word: ', ' '.join(word_list).upper())
 
         user_letter = input('Guess a letter: ').lower()
-
+        if user_letter in used_letters:
+            print("You have already used this letter")
+        if user_letter not in word_letters:
+            print("That's not a valid letter")
         if user_letter in alphabet - used_letters:
             used_letters.add(user_letter)
         if user_letter in word_letters:
             word_letters.remove(user_letter)
-        elif user_letter in used_letters:
-            print("You have already used this letter")
-        else:
-            print("Invalid")
+
+
 
 
 
